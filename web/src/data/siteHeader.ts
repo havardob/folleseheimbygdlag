24e9791aaf96bcba930c8ca34b8f15da88865876
Håvard Brynjulfsen <havardob@hotmail.com>
2025-03-11 22:56:33 +0100

    initial astro app commit

---
+++ web/src/data/siteHeader.ts
@@ -0,0 +1,16 @@
+export const siteHeader = {
+    nav: [
+        {
+            href: "/om-oss",
+            label: "Om oss"
+        },
+        {
+            href: "/historie",
+            label: "Historie"
+        },
+        {
+            href: "/bli-medlem",
+            label: "Bli medlem"
+        }
+    ]
+}
---
+++ web/src/data/siteSettings.js
@@ -0,0 +1,7 @@
+export const siteSettings = {
+  metaTitle: "Follese Heimbygdlag",
+  metaDescription:
+    "Velkommen til nettsiden for Follese Heimbygdlag, ditt lokale historielag på Follese, Askøy.",
+  metaUrl: "https://folleseheimbygdlag.no/",
+  metaImageUrl: "/featured-image.jpg",
+};
