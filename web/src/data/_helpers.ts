04f2277028d08dd5dee3c57c2e5ea74601b995c4
Håvard Brynjulfsen <havardob@hotmail.com>
2025-03-22 16:38:15 +0100

    adds news to the frontpage

---
+++ web/src/data/_helpers.ts
@@ -0,0 +1,25 @@
+export const formatDate = function (date: any) {
+    const tempDate = new Date(date);
+    const day = tempDate.getDate();
+    const month = tempDate.getMonth();
+    const year = tempDate.getFullYear();
+
+    const monthNames = [
+        'januar',
+        'februar',
+        'mars',
+        'april',
+        'mai',
+        'juni',
+        'juli',
+        'august',
+        'september',
+        'oktober',
+        'november',
+        'desember'
+    ]
+
+    const newDate = `${day}. ${monthNames[month]} ${year}`;
+
+    return newDate;
+}
