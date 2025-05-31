733c7458f8d3e7613519f58df14af55fa9079c2a
Håvard Brynjulfsen <havardob@hotmail.com>
2025-05-31 16:13:36 +0200

    adds weather block

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -2,12 +2,12 @@
 import { defineConfig } from "astro/config";
 import sitemap from '@astrojs/sitemap';
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://folleseheimbygdlag.no",
-  integrations: [
-    sitemap()
-  ],
+  integrations: [sitemap(), react()],
   devToolbar: {
     enabled: false
   }
