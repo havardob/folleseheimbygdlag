6db3c179c107b7417cd4dfca4c27f250e6bd43fb
Håvard Brynjulfsen <havardob@hotmail.com>
2025-06-19 08:40:53 +0200

    remove token

--- web/src/data/_sanityClient.ts
+++ web/src/data/_sanityClient.ts
@@ -3,7 +3,6 @@ import { createClient } from "@sanity/client";
 const config = {
     projectId: import.meta.env.SANITY_STUDIO_PROJECT_ID || 'qd5ip3k2',
     dataset: import.meta.env.SANITY_STUDIO_DATASET || '',
-    token: import.meta.env.SANITY_STUDIO_TOKEN || '',
     apiVersion: '2023-07-06',
     useCdn: false,
 }
